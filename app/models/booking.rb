7aa656eeaa2c6859830609fb8f3bcb2e2352464d
floriansaubin <saubinflorian@gmail.com>
2021-03-08 16:18:16 +0100

    adding valiations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :piano
   belongs_to :user
+
+  validates :content, presence: true
 end
--- app/models/piano.rb
+++ app/models/piano.rb
@@ -1,4 +1,6 @@
 class Piano < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  validates :name, presence: true
 end
