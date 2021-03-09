796707184ed00668ecffea6c824da6c9b60c6780
floriansaubin <saubinflorian@gmail.com>
2021-03-09 14:16:24 +0100

    WOrk on it

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :pianos do
-    resources :bookings
+    resources :bookings, only: %i[index new create edit update]
   end
+  resources :bookings, only: %i[destroy]
 end
