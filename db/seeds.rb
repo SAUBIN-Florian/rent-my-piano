2beab866a1535ec8465a560e3ad4ab23266b6809
sylvainmaron <sylvain.maron@essca.eu>
2021-03-10 15:36:37 +0100

    e

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-puts "Cleaning DB"
-ActiveStorage::Attachment.all.find_each do |a|
-    a.purge
-end
-ActiveStorage::Blob.all.find_each do |b|
-    b.purge
-end
 Piano.destroy_all
 Booking.destroy_all
 User.destroy_all
+Piano.create({name: 'B1', price: 50, brand: 'Korg', photo: Cloudinary::Uploader.download('v1615379237/7ihmynbdvtpn9livtvf3upe22cnl.jpg')})
