d846a4a480b9a0aff5a1db575177124e0a110e13
sylvainmaron <sylvain.maron@essca.eu>
2021-03-11 15:40:39 +0100

    changes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,5 @@
+const { environment } = require('@rails/webpacker')
+
 const webpack = require('webpack');
   // Preventing Babel from transpiling NodeModules packages
   environment.loaders.delete('nodeModules');
